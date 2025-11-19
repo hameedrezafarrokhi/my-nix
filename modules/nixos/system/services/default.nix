96d989415d0e70b3ce8ebb213aacc759acf5f727
hameedrezafarrokhi <hameedrezafarrokhi@gmail.com>
2025-11-20 03:16:29 +0330

    2025-11-20_03-16-29

--- modules/nixos/system/services/default.nix
+++ modules/nixos/system/services/default.nix
@@ -8,12 +8,18 @@
 
     services = {
 
+      orca = {
+        enable = false;
+        package = pkgs.orca;
+      };
+
       # GNOME STUFF
       gnome.localsearch.enable = true;
       gnome.tinysparql.enable = true;
       gnome.glib-networking.enable = true;
       gnome.sushi.enable = true;
       sysprof.enable = true;
+
       gvfs = {
         enable = true;
         package = pkgs.gnome.gvfs;
